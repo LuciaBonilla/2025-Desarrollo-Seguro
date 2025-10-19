15ed1610e38af95b3c740707828a223d29490424
lucia.bonilla <lucia.bonilla@correo.ucu.edu.uy>
2025-10-19 20:03:36 -0300

    Etapa inicial de test de Template Injection

--- services/backend/test/services/authService.test.ts
+++ services/backend/test/services/authService.test.ts
@@ -349,3 +349,87 @@ describe('AuthService.generateJwt', () => {
   });
 
 });
+
+/*
+  Repertorio de tests para verificar que las vulnerabilidades halladas en el
+  práctico 2 fueron mitigadas.
+*/
+describe('AuthService.security', () => {
+  const OLD_ENV = process.env;
+  beforeEach (() => {
+    jest.resetModules();
+    jest.clearAllMocks();
+
+  });
+
+    /*
+      Descripción:
+      Prueba unitaria que valida la mitigación del Template Code Injection
+      identificado en la funcionalidad de envío de correo al crear un nuevo
+      usuario.
+
+      Procedimiento:
+      Para validar la mitigación se verifica que el contenido del correo de
+      creación de cuenta no tenga el resultado de la ejecución del código pasado
+      como entrada de usuario, o bien que el correo no sea creado ante esa
+      entrada maliciosa.
+
+      Entrada: Un usuario con first_name: '<%= {4*4} %>'
+
+      Resultado positivo: Dada la entrada, no se crea un correo con el valor 16,
+      lo cual significa que el código NO es ejecutado en el lado del servidor.
+
+      Resultado negativo: Dada la entrada, se crea un correo con el valor 16, lo
+      cual significa que el código SÍ es ejecutado en el lado del servidor.
+    */
+    it('No Template Code Injection in create user mail', async () => {
+    const user  = {
+      id: 'user-123',
+      email: 'a@a.com',
+      password: 'password123',
+      first_name: '<%= {4*4} %>',
+      last_name: 'Last',
+      username: 'username',
+    } as User;
+
+    // mock no user exists
+    const selectChain = {
+      where: jest.fn().mockReturnThis(),
+      orWhere: jest.fn().mockReturnThis(),
+      first: jest.fn().mockResolvedValue(null) // No existing user
+    };
+    // Mock the database insert
+    const insertChain = {
+      returning: jest.fn().mockResolvedValue([user]),
+      insert: jest.fn().mockReturnThis()
+    };
+    mockedDb
+    .mockReturnValueOnce(selectChain as any)
+    .mockReturnValueOnce(insertChain as any);
+
+    // Call the method to test
+    await AuthService.createUser(user);
+
+    // Verify the database calls
+    expect(insertChain.insert).toHaveBeenCalledWith({
+      email: user.email,
+      password: expect.any(String),
+      first_name: user.first_name,
+      last_name: user.last_name,
+      username: user.username,
+      activated: false,
+      invite_token: expect.any(String),
+      invite_token_expires: expect.any(Date)
+    });
+
+    expect(nodemailer.createTransport).toHaveBeenCalled();
+    expect(nodemailer.createTransport().sendMail).toHaveBeenCalledWith({
+      to: user.email,
+      subject: 'Activate your account',
+      html: expect.stringContaining('Click <a href="')
+    });
+
+
+  }
+  );
+});
